ad3ae4456899d027e512b1e7425ebd1d7279714a
jklovetocode <106761531+jklovetocode@users.noreply.github.com>
2023-08-30 22:06:24 +0700

    context

--- context/contractContext.tsx
+++ context/contractContext.tsx
@@ -1,7 +1,8 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { ethers } from 'ethers'
-import artifactContract from '../artifacts/contract.json'
-import { Contract } from '../utils/contractAddress'
+import artifactContractNFT from '../artifacts/contracts/JUMPER.sol/JUMPER.json'
+import { ContractNFT } from '../utils/contractAddress'
+import { JUMPER } from '../typechain-types'
 import {
   toEtherandFixFloatingPoint,
   toWei,
@@ -9,15 +10,35 @@ import {
   toEtherFloatingPoint,
   toFixUnits,
 } from '../utils/UnitInEther'
+import { useAccount, useNetwork } from 'wagmi'
+import { ChainIDAvalanchefuji, ChainIDFantomTestnet, ChainIDMumbai, FindRPCByChainID, findContractNFT,findGas } from '../utils/FindHelper'
 
 interface IContract {
-  sendTxFaucet: () => Promise<string | void>
-  data: string
+  sendTxMintNFT: () => Promise<string>
+  sendTxBridgeNFT: (destinationChain: string, tokenID: number) => Promise<string>
+  getDataNFTAvalanchefuji: () => Promise<void>
+  getDataNFTFantom: () => Promise<void>
+  getDataNFTPolygon: () => Promise<void>
+  loadingNFTDataFantom: boolean
+  loadingNFTDataPolygon: boolean
+  loadingNFTDataAvalanchefuji: boolean
+  userNFTAvalanchefuji: TypeNFT[]
+  userNFTPolygon: TypeNFT[]
+  userNFTFantom: TypeNFT[]
 }
 
 export const ContractContext = createContext<IContract>({
-  sendTxFaucet: async () => {},
-  data: '',
+  sendTxMintNFT: async () => '',
+  sendTxBridgeNFT: async () => '',
+  getDataNFTAvalanchefuji: async () => {},
+  getDataNFTFantom: async () => {},
+  getDataNFTPolygon: async () => {},
+  loadingNFTDataFantom: false,
+  loadingNFTDataPolygon: false,
+  loadingNFTDataAvalanchefuji: false,
+  userNFTAvalanchefuji: [],
+  userNFTPolygon: [],
+  userNFTFantom: [],
 })
 
 interface ChildrenProps {
@@ -25,55 +46,248 @@ interface ChildrenProps {
 }
 
 export const ContractProvider = ({ children }: ChildrenProps) => {
+ 
+  const { chain } = useNetwork()
+  const { isDisconnected,address } = useAccount()
+  
   const [initialLoading, setInitialLoading] = useState(true)
+  let providerWindow: ethers.providers.Web3Provider
+  let providerRPCAvalanchefuji = new ethers.providers.JsonRpcProvider(FindRPCByChainID(ChainIDAvalanchefuji))
+  let providerRPCMumbai = new ethers.providers.JsonRpcProvider(FindRPCByChainID(ChainIDMumbai))
+  let providerRPCFantomTestnet = new ethers.providers.JsonRpcProvider(FindRPCByChainID(ChainIDFantomTestnet))
 
-  const [data, setData] = useState('')
-
-  const getContract = () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum as any)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(Contract, artifactContract.abi, signer)
-
-    return contract
+  if (typeof window !== 'undefined') {
+    try {
+      providerWindow = new ethers.providers.Web3Provider(window.ethereum as any)
+    } catch (error) {}
   }
+  const getNFTContractAvalanchefuji  = new ethers.Contract(
+      ContractNFT.Avalanche.contractAddress,
+      artifactContractNFT.abi,
+      providerRPCAvalanchefuji
+    ) as JUMPER
+
+
+  const getNFTContractPolygon = new ethers.Contract(
+      ContractNFT.Polygon.contractAddress,
+      artifactContractNFT.abi,
+      providerRPCMumbai
+    ) as JUMPER
+
+  const getNFTContractFantomTestnet = new ethers.Contract(
+      ContractNFT.Fantom.contractAddress,
+      artifactContractNFT.abi,
+      providerRPCFantomTestnet
+    ) as JUMPER
+
+  const [loadingNFTDataAvalanchefuji, setLoadingNFTDataAvalanchefuji] = useState(false)
+  const [loadingNFTDataPolygon, setLoadingNFTDataPolygon] = useState(false)
+  const [loadingNFTDataFantom, setLoadingNFTDataFantom] = useState(false)
+  const [userNFTAvalanchefuji, setUserAvalanchefuji] = useState<TypeNFT[]>([])
+  const [userNFTPolygon , setUserPolygon ] = useState<TypeNFT[]>([])
+  const [userNFTFantom, setUserFantom] = useState<TypeNFT[]>([])
 
   useEffect(() => {
-    if (!window.ethereum) return alert('Please install metamask')
-    queryData()
     setInitialLoading(false)
+    addlistenerEvents()
+    getDataNFTFantom()
+    getDataNFTPolygon()
+    getDataNFTAvalanchefuji()
   }, [])
 
-  const sendTxFaucet = async () => {
-    if (!window.ethereum) return console.log('Please install metamask')
+  const sendTxMintNFT = async () => {
     try {
-      const contract = getContract()
-      const transactionHash = await contract.addNumber()
-      console.log(transactionHash.hash)
+      if (isDisconnected || (chain?.id == undefined) || address == undefined) throw new Error('disconnect wallet')
+      const signer = providerWindow.getSigner()
+      const contractNFT = new ethers.Contract(
+        findContractNFT(chain.id),
+        artifactContractNFT.abi,
+        signer
+      ) as JUMPER
+      const transactionHash = await contractNFT.safeMint(address)
       await transactionHash.wait()
-      alert(`Tx : ${transactionHash.hash}`)
       return transactionHash.hash
     } catch (error: any) {
-      // throw new Error(error.reason)
-      alert(error)
+       if (error.reason) {
+        throw new Error(error.reason)
+      } else if (error.data?.message) {
+        throw new Error(error.data.message)
+      } else {
+        throw new Error(error)
+      }
+  }
+}
+
+  const sendTxBridgeNFT = async (destinationChain:string,tokenID:number) => {
+    try {
+      if (isDisconnected || chain?.id == undefined || address == undefined)
+        throw new Error('disconnect wallet')
+      const signer = providerWindow.getSigner()
+      const contractNFT = new ethers.Contract(
+        findContractNFT(chain.id),
+        artifactContractNFT.abi,
+        signer
+      ) as JUMPER
+      const transactionHash = await contractNFT.bridge(destinationChain,tokenID,{
+        value: toWei(findGas(chain.id))
+      })
+      await transactionHash.wait()
+      await Promise.all([
+        getDataNFTFantom(),
+        getDataNFTPolygon(),
+        getDataNFTAvalanchefuji(),
+      ])
+      return transactionHash.hash
+    } catch (error: any) {
+      if (error.reason) {
+        throw new Error(error.reason)
+      } else if (error.data?.message) {
+        throw new Error(error.data.message)
+      } else {
+        throw new Error(error)
+      }
     }
   }
 
-  const queryData = async () => {
-    if (!window.ethereum) return console.log('Please install metamask')
+  const getDataNFTAvalanchefuji = async () => {
     try {
-      const contract = getContract()
-      const getData = await contract.getNumber()
-      setData(toFixUnits(getData, '0'))
+      if (isDisconnected || (chain?.id == undefined) || address == undefined) throw new Error('disconnect wallet')
+      setLoadingNFTDataAvalanchefuji(true)
+      setUserAvalanchefuji([])
+      const ownerNFTAvalanchefuji=  await getNFTContractAvalanchefuji.walletOfOwner(address)
+      
+      let tempDataNFTAvalanchefuji: TypeNFT[] = []
+      await Promise.all(
+         ownerNFTAvalanchefuji.map(async (item) => {
+          const dataNftStatus = await getNFTContractAvalanchefuji.nftStatus(
+            item
+          )
+          const struct: TypeNFT = {
+            tokenID: item.toNumber(),
+            level: dataNftStatus.level.toNumber(),
+            point: dataNftStatus.point.toNumber(),
+            born: dataNftStatus.born.toNumber(),
+            codePic: dataNftStatus.codePic.toNumber(),
+            avax: dataNftStatus.avax.toNumber(),
+            fantom: dataNftStatus.fantom.toNumber(),
+            matic: dataNftStatus.matic.toNumber(),
+          }
+          tempDataNFTAvalanchefuji.push(struct)
+        }))
+
+      setUserAvalanchefuji(tempDataNFTAvalanchefuji)
+
+      setLoadingNFTDataAvalanchefuji(false)
     } catch (error: any) {
-      // throw new Error(error.reason)
-      // alert(error)
+      setLoadingNFTDataAvalanchefuji(false)
+      console.log(error)
     }
   }
+  const getDataNFTFantom = async () => {
+    try {
+      if (isDisconnected || (chain?.id == undefined) || address == undefined) throw new Error('disconnect wallet')
+      setLoadingNFTDataFantom(true)
+      setUserFantom([])
+      const ownerNFTFantomTestnet=  await getNFTContractFantomTestnet.walletOfOwner(address)
+      
+      let tempDataNFTFantomTestnet: TypeNFT[] = []
+      await Promise.all(
+         ownerNFTFantomTestnet.map(async (item) => {
+          const dataNftStatus = await getNFTContractFantomTestnet.nftStatus(
+            item
+          )
+          const struct: TypeNFT = {
+            tokenID: item.toNumber(),
+            level: dataNftStatus.level.toNumber(),
+            point: dataNftStatus.point.toNumber(),
+            born: dataNftStatus.born.toNumber(),
+            codePic: dataNftStatus.codePic.toNumber(),
+            avax: dataNftStatus.avax.toNumber(),
+            fantom: dataNftStatus.fantom.toNumber(),
+            matic: dataNftStatus.matic.toNumber(),
+          }
+          tempDataNFTFantomTestnet.push(struct)
+        }))
+
+      setUserFantom(tempDataNFTFantomTestnet)
+
+      setLoadingNFTDataFantom(false)
+    } catch (error: any) {
+      setLoadingNFTDataFantom(false)
+      console.log(error)
+    }
+  }
+
+  const getDataNFTPolygon = async () => {
+    try {
+      if (isDisconnected || (chain?.id == undefined) || address == undefined) throw new Error('disconnect wallet')
+      setLoadingNFTDataPolygon(true)
+      setUserPolygon([])
+      const ownerNFTPolygon=  await getNFTContractPolygon.walletOfOwner(address)
+      
+      let tempDataNFTPolygon: TypeNFT[] = []
+      await Promise.all(
+         ownerNFTPolygon.map(async (item) => {
+          const dataNftStatus = await getNFTContractPolygon.nftStatus(
+            item
+          )
+          const struct: TypeNFT = {
+            tokenID: item.toNumber(),
+            level: dataNftStatus.level.toNumber(),
+            point: dataNftStatus.point.toNumber(),
+            born: dataNftStatus.born.toNumber(),
+            codePic: dataNftStatus.codePic.toNumber(),
+            avax: dataNftStatus.avax.toNumber(),
+            fantom: dataNftStatus.fantom.toNumber(),
+            matic: dataNftStatus.matic.toNumber(),
+          }
+          tempDataNFTPolygon.push(struct)
+        }))
+
+      setUserPolygon(tempDataNFTPolygon)
+
+      setLoadingNFTDataPolygon(false)
+    } catch (error: any) {
+      setLoadingNFTDataPolygon(false)
+      console.log(error)
+    }
+  }
+
+ const addlistenerEvents = async () => {
+   try {
+     if (window.ethereum != undefined) {
+       //@ts-ignore
+       window.ethereum.on('accountsChanged', () => {
+         window.location.reload()
+       })
+       interface ConnectInfo {
+         chainId: string
+       }
+
+       //@ts-ignore
+       window.ethereum.on('chainChanged', (_chainId) => {
+         window.location.reload()
+       })
+     }
+   } catch (error) {}
+ }
+
+
+
   return (
     <ContractContext.Provider
       value={{
-        sendTxFaucet,
-        data,
+        sendTxMintNFT,
+        sendTxBridgeNFT,
+        getDataNFTAvalanchefuji,
+        getDataNFTFantom,
+        getDataNFTPolygon,
+        loadingNFTDataAvalanchefuji,
+        loadingNFTDataFantom,
+        loadingNFTDataPolygon,
+        userNFTFantom,
+        userNFTAvalanchefuji,
+        userNFTPolygon
       }}
     >
       {!initialLoading && children}
--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -1,1 +1,11 @@
-export const Contract = '0xB0052797DF1FFdd0FB73d10DCBD76fdf98bd5792'
+export const ContractNFT = {
+  Fantom: {
+    contractAddress: '0x2D8d1b75bea224370cCdCC5F65E2329f0E85d2f6',
+  },
+  Avalanche: {
+    contractAddress: '0x16939Ed9fc9a49997113b8A7B7930AbF2dAD3506',
+  },
+  Polygon: {
+    contractAddress: '0xd2085F9aa729755f323B9b8083bB97CC8903fb29',
+  },
+}
