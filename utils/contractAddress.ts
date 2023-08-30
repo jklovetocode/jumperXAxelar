9e2f9e88d3a16e9952d9d4dfb604f864a8a4fc0c
jklovetocode <106761531+jklovetocode@users.noreply.github.com>
2023-08-30 22:29:19 +0700

    new re deployed contract

--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -1,11 +1,11 @@
 export const ContractNFT = {
   Fantom: {
-    contractAddress: '0x2D8d1b75bea224370cCdCC5F65E2329f0E85d2f6',
+    contractAddress: '0xC7344449f02E96D88f751eDFeaA5519afB25ef55',
   },
   Avalanche: {
-    contractAddress: '0x16939Ed9fc9a49997113b8A7B7930AbF2dAD3506',
+    contractAddress: '0xA8Ece3359e0fe7f6d781AeF302582a08d52AE552',
   },
   Polygon: {
-    contractAddress: '0xd2085F9aa729755f323B9b8083bB97CC8903fb29',
+    contractAddress: '0xdf85249aa8f5be1C882d7b86486Ca8715ADdeeFA',
   },
 }
